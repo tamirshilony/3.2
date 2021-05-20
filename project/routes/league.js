64ea6de5168cb9cdd3cf85a2f0f641a17264d615
tamirshilony <tamirshilony@gmail.com>
2021-05-20 15:38:44 +0300

    start

--- project/routes/league.js
+++ project/routes/league.js
@@ -2,13 +2,13 @@ var express = require("express");
 var router = express.Router();
 const league_utils = require("./utils/league_utils");
 
-router.get("/getDetails", async (req, res, next) => {
-  try {
-    const league_details = await league_utils.getLeagueDetails();
-    res.send(league_details);
-  } catch (error) {
-    next(error);
-  }
+router.get("/getDetails", async(req, res, next) => {
+    try {
+        const league_details = await league_utils.getLeagueDetails();
+        res.send(league_details);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
