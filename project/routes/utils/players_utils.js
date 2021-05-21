52b20364bc0067eec21a7efb30ccbcfdd2fc90f9
Ofer Avin <oferavin32@gmail.com>
2021-05-21 12:48:58 +0300

    teams

--- project/routes/utils/players_utils.js
+++ project/routes/utils/players_utils.js
@@ -1,18 +1,18 @@
 const axios = require("axios");
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
-// const TEAM_ID = "85";
+const TEAM_ID = "85";
 
-async function getPlayerFullDetailsById(player_id){
-    const player_info = axios.get(`${api_domain}/players/${id}`, {
-        params: {
-            api_token: process.env.api_token,
-        },
-    })
-}
+// async function getPlayerFullDetailsById(player_id){
+//     const player_info = axios.get(`${api_domain}/players/${id}`, {
+//         params: {
+//             api_token: process.env.api_token,
+//         },
+//     })
+// }
 
 async function getPlayerIdsByTeam(team_id) {
     let player_ids_list = [];
-    const team = await axios.get(`${api_domain}/teams/${team_id}`, {
+    const team = await axios.get(`${api_domain}/teams/${TEAM_ID}`, {
         params: {
             include: "squad",
             api_token: process.env.api_token,
