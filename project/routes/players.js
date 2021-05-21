03109b28efacffa3656f53d2ddb63133a0faedb8
tamirshilony <tamirshilony@gmail.com>
2021-05-21 16:28:19 +0300

    f

--- project/routes/players.js
+++ project/routes/players.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-// const DButils = require("./utils/DButils");
 const players_utils = require("./utils/players_utils");
 
 router.get("/playerDetails/:playerId", async(req, res, next) => {
