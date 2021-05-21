df18601d30d1548e07a20ac955a10e1ec196273e
Ofer Avin <oferavin32@gmail.com>
2021-05-21 14:57:55 +0300

    ooof

--- project/main.js
+++ project/main.js
@@ -51,6 +51,7 @@ const auth = require("./routes/auth");
 const users = require("./routes/users");
 const league = require("./routes/league");
 const teams = require("./routes/teams");
+const players = require("./routes/players");
 
 //#endregion
 
@@ -76,9 +77,9 @@ app.get("/alive", (req, res) => res.send("I'm alive"));
 
 // Routings
 app.use("/users", users);
+// app.use("/players", players);
 app.use("/league", league);
 app.use("/teams", teams);
-
 app.use(auth);
 
 app.use(function(err, req, res, next) {
